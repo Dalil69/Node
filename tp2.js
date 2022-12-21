c0c5e568c1f37b08c990c426b02799a05d00f239
John Doe <johndoe@example.com>
2022-12-21 10:52:49 +0100

    initial commit

--- tp2.js
+++ tp2.js
@@ -1,22 +1,22 @@
 //1 Écrivez un programme Node.js qui lit un fichier et imprime son contenu dans la console.
-const fs = require('fs');
+const s = require('fs');
 
-fs.readFile("fichier.js", "utf-8", (err, data) => {
+s.readFile("fichier.js", "utf-8", (err, data) => {
     if (err) {
         console.log(err)
         return
     }
-    console.log(data)
+    //console.log(data)
 })
 
 //2 Écrivez un programme Node.js qui lit un fichier, modifie son contenu et réécrit le contenu modifié dans le fichier.
-const fs = require('fs');
+//const fs = require('fs');
 
-fs.readFile("fichier2.js", )
+//fs.readFile("fichier2.js", )
 
 //3 Écrivez un programme Node.js qui utilise le module http pour créer un serveur simple qui sert un fichier statique.
 
-const { http2serverResponse} = require('http2');
+/*const { http2serverResponse} = require('http2');
 
 http.createServer((htttRequest, httpResponse)=> {
 
@@ -33,5 +33,40 @@ http.createServer((htttRequest, httpResponse)=> {
 
     })
 }).listen(3000, () => {
-    console.log('server web listening on port 3000')
-});
+    //console.log('server web listening on port 3000')
+});*/
+
+//4 Écrivez un programme Node.js qui utilise le module fs pour lister les fichiers d'un répertoire.
+/*const fs = require("fs").promises;
+
+async function main(){
+    await findSalesFiles("Dossier");
+}
+
+main();
+
+async function findSalesFiles(folderName) {
+    const rp = await fs.readdir(folderName);
+    //console.log(rp);
+}*/
+
+//5 Écrivez un programme Node.js qui utilise le module fs pour créer un nouveau fichier et y écrire des données.
+const fs = require("fs").promises;
+const path = require("path");
+
+async function main() {
+    const Bonjourrr = path.join(__dirname, "Bonjour");
+    const Bonjour = path.join(__dirname, "Aurevoir")
+
+
+try {
+    await fs.mkdir(Bonjour);
+} catch {
+    console.log('${Bonjour} already exists.')
+}
+const ok =  await findSalesFiles(Bonjourrr);
+
+await fs.writeFile(path.join(Bonjour, "report.json"), "");
+console.log(`Sales report written to ${Bonjour}`);
+}
+
