48eeccbe4793c4c3ba64e7c7b2dd4e20da16ff5d
John Doe <johndoe@example.com>
2022-12-22 12:41:27 +0100

    mise en place des conditions

--- guessnbr.js
+++ guessnbr.js
@@ -1,7 +1,7 @@
 const readline = require('readline'); //sert aux entrées clavier
 
-const randomNumber = Math.round(Math.random() * 10); //dans ma variable randomNumber je stock Math random qui choisis un nombre au hasard entre la tranche donner
-const lives = 1;
+const randomNumber = Math.round(Math.random() * 5); //dans ma variable randomNumber je stock Math random qui choisis un nombre au hasard entre la tranche donner
+const lives = 5;
 //Gére les interactions avec readline
 const terminal = readline.createInterface(
 {
@@ -9,4 +9,22 @@ const terminal = readline.createInterface(
   output : process.stdout
 });
 
+terminal.setPrompt('Devine le prix! (0-10): ');//Ici setPrompt me sert a reécrir l'ennoncer dans la console
+terminal.prompt();
+terminal.on('line', function(answer) //la fonction contient le parametre reponse
+{
+  const answerNum = parseInt(answer); //la varibale answerNum est lier a la reponse donc answer
+//Si la reponse est superrieur au nombre choisi alors il sera signaler qu'il faut baisser le nombre
+  if (answerNum > randomNumber)
+  {
+    console.log('Plus bas !');
+    console.log('Tu as '+ (lives-1)+' vies');
+  }
+//Sinon si la reponse est inferrieur au nombre choisi alors il sera signaler qu'il faut monter le nombre
+  else if (answerNum < randomNumber){
+  
+        console.log('Plus haut !');
+        console.log('Tu as '+ (lives-1) +' lives left');
+  }
 
+ 
