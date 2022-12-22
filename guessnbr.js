93f7ebcfe6b138b9f1dd7ae4797f52c75e9a4026
John Doe <johndoe@example.com>
2022-12-22 16:28:52 +0100

    presque fini

--- guessnbr.js
+++ guessnbr.js
@@ -2,6 +2,7 @@ const readline = require('readline'); //sert aux entrées clavier
 
 const randomNumber = Math.round(Math.random() * 5); //dans ma variable randomNumber je stock Math random qui choisis un nombre au hasard entre la tranche donner
 const lives = 5;
+
 //Gére les interactions avec readline
 const terminal = readline.createInterface(
 {
@@ -12,19 +13,58 @@ const terminal = readline.createInterface(
 terminal.setPrompt('Devine le prix! (0-10): ');//Ici setPrompt me sert a reécrir l'ennoncer dans la console
 terminal.prompt();
 terminal.on('line', function(answer) //la fonction contient le parametre reponse
-{
+
+{ 
+
+
   const answerNum = parseInt(answer); //la varibale answerNum est lier a la reponse donc answer
 //Si la reponse est superrieur au nombre choisi alors il sera signaler qu'il faut baisser le nombre
+
   if (answerNum > randomNumber)
   {
+    
     console.log('Plus bas !');
-    console.log('Tu as '+ (lives-1)+' vies');
-  }
+    console.log('Tu as '+ lives+' vies');
+    
+  } 
 //Sinon si la reponse est inferrieur au nombre choisi alors il sera signaler qu'il faut monter le nombre
   else if (answerNum < randomNumber){
-  
+
         console.log('Plus haut !');
-        console.log('Tu as '+ (lives-1) +' lives left');
+        console.log('Tu as '+ lives +' lives left');
+        
+  }
+//si c'est egale alors il gagne
+   else if (answerNum === randomNumber)
+  {
+    console.log('GAGNER!!!');
+    console.log('Tu gagne avec '+lives+ ' vie');
+    process.exit(0);
+    
+  }
+//si autre qu'un nombre est inserer il y a une erreur
+  else
+  {
+    console.log("Ce n'est pas reconnue comme un nombre !");
+    console.log('Tu as '+lives+' vies');
+    
   }
 
- 
+  //si les vies sont = a 0 c'est perdu
+  if (lives == 0)
+  {
+    console.log('PERDU!!! Le perdant reçoit un gage donner par les autres joueurs.');
+    process.exit(0);
+    
+  } 
+
+
+
+
+
+  terminal.prompt();
+});
+
+
+
+
